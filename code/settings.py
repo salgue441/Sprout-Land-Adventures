c78277b309ca806365ccd48f18550c2934581a41
salgue441 <carlossalguero441@gmail.com>
2022-12-16 22:40:00 -0600

    New settings

--- code/settings.py
+++ code/settings.py
@@ -8,7 +8,7 @@ TILE_SIZE = 64
 # overlay positions
 OVERLAY_POSITIONS = {
     'tool': (40, SCREEN_HEIGHT - 15),
-    'seed': (70, SCREEN_HEIGHT - 5)
+    'seed': (100, SCREEN_HEIGHT - 15)
 }
 
 PLAYER_TOOL_OFFSET = {
@@ -30,4 +30,26 @@ LAYERS = {
     'house top': 8,
     'fruit': 9,
     'rain drops': 10
+}
+
+APPLE_POS = {
+    'Small': [(18, 17), (30, 37), (12, 50), (30, 45), (20, 30), (30, 10)],
+    'Large': [(30, 24), (60, 65), (50, 50), (16, 40), (45, 50), (42, 70)]
+}
+
+GROW_SPEED = {
+    'corn': 1,
+    'tomato': 0.7
+}
+
+SALE_PRICES = {
+    'wood': 4,
+    'apple': 2,
+    'corn': 10,
+    'tomato': 20
+}
+
+PURCHASE_PRICES = {
+    'corn': 4,
+    'tomato': 5
 }
